66732234385e0c71ac3714957fca945ff9d3bc48
Vazzzx <lucasroyale0805@gmail.com>
2023-06-27 22:14:18 -0300

    envio

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Calculadora from '../components/calc'
+
+
+export default function Home() {
+  return (
+    <div className="bg-[#F4F4F4] min-h-screen flex flex-col justify-center">
+    <Calculadora />
+  </div>
+  );
+}
